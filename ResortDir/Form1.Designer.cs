c676bcc1b536113e1eb08ee8f377346dd8182b8c
Администратор <@NEXT-NOTEBOOK>
2017-05-23 14:57:29 +0300

    Add project files.

---
+++ ResortDir/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ResortDir
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(598, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel6,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 229);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(688, 24);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(99, 19);
+            this.toolStripStatusLabel1.Text = "Найдено папок -";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.AutoSize = false;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 19);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(83, 19);
+            this.toolStripStatusLabel5.Text = "Обработано -";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.AutoSize = false;
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(80, 19);
+            this.toolStripStatusLabel6.Text = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(105, 19);
+            this.toolStripStatusLabel3.Text = "Корневая папка - ";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(118, 19);
+            this.toolStripStatusLabel4.Text = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Image = global::ResortDir.Properties.Resources.refresh;
+            this.btnRefresh.Location = new System.Drawing.Point(616, 60);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(60, 42);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolTip1.SetToolTip(this.btnRefresh, "Обновить");
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Image = global::ResortDir.Properties.Resources.media_play_green;
+            this.btnStart.Location = new System.Drawing.Point(616, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(60, 42);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolTip1.SetToolTip(this.btnStart, "Запуск");
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(5, 8);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(451, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 10;
+            this.progressBar1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Location = new System.Drawing.Point(81, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 39);
+            this.panel1.TabIndex = 5;
+            this.panel1.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 253);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(704, 292);
+            this.Name = "MainForm";
+            this.Text = "RemoveSubdir";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ResortDir/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResortDir
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Рабочая папка пролграммы
+        /// </summary>
+        private string appDir = Directory.GetCurrentDirectory();
+
+        /// <summary>
+        /// Запуск перемещения 
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var processedDirCount = 0;                                                  /// Количество обработанных папок
+            var listRootDir = Directory.GetDirectories(appDir);                         /// Список папок в рабочей папке
+
+            RefreshInfo();
+            var totalDir = Directory.GetDirectories(appDir).Length;                     /// Общее количество папок в рабочей папке
+
+            Task th = new Task(() =>
+            {
+                progressBar1.Invoke(new Action(() =>
+                {
+                    progressBar1.Value = 0;
+                    progressBar1.Visible = panel1.Visible = true;
+                }));
+                
+                var totalProcessedDirCount = 0;
+
+                foreach (string childDir in listRootDir)
+                {
+                    totalProcessedDirCount++;
+                    progressBar1.Invoke(new Action(() => progressBar1.Value = (int)((float)totalProcessedDirCount/totalDir*100)));
+                    listBox1.Invoke(new Action(() => listBox1.Items.Add(childDir)));
+                    var listChildDir = Directory.GetDirectories(childDir);              /// Получение подпапок для папки из корневой директории 
+                    if (listChildDir.Length >= 1)
+                    {
+                        /// В папке из корневой директории есть подпапки. Обрабатываем
+                        processedDirCount++;
+                        foreach (string childSubDir in listChildDir)
+                        {
+                            var listFile = Directory.GetFiles(childSubDir);             /// Получение списка файлов в папке
+                            foreach (string sourceFile in listFile)
+                            {
+                                try
+                                {
+                                    if (File.Exists(childDir + "\\" + Path.GetFileName(sourceFile)))                            /// Проверка на существование такого же файла в папке назначения
+                                    {
+                                        var i = 1;
+
+                                        while (i != -1)
+                                        {
+                                            if (!File.Exists(childDir + "\\" + Path.GetFileNameWithoutExtension(sourceFile) + "_" + i.ToString() + Path.GetExtension(sourceFile)))
+                                            {
+                                                Directory.Move(sourceFile, childDir + "\\" + Path.GetFileNameWithoutExtension(sourceFile) + "_" + i.ToString() + Path.GetExtension(sourceFile));      /// Копируем с изменением имени при наличии такого же файла
+                                                i = -1;
+                                            }
+                                            else i++;
+                                        }
+                                    }
+                                    else Directory.Move(sourceFile, childDir + "\\" + Path.GetFileName(sourceFile));            /// Перемещение файла
+                                }
+                                catch (Exception)
+                                {
+                                    throw;
+                                }
+                            }
+                            if (Directory.GetFiles(childSubDir).Length == 0)
+                                Directory.Delete(childSubDir);                          /// Удаление пустых после перемещения папок
+                        }
+                        toolStripStatusLabel6.Text = processedDirCount.ToString();      /// Вывод количества обработанных папок
+                    }
+                    Thread.Sleep(100);
+                }
+                progressBar1.Invoke(new Action(() => progressBar1.Visible = panel1.Visible = false)); 
+            });
+            th.Start();             /// Запуск потока обработки папок 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshInfo();
+        }
+
+        /// <summary>
+        /// Обновление отображаемых данных в окне
+        /// </summary>
+        private void RefreshInfo()
+        {
+            #region Вывод информации в строку статуса
+            listBox1.Items.Clear();
+            toolStripStatusLabel4.Text = appDir;                                                /// Вывод рабочей папки                                              
+            toolStripStatusLabel2.Text = Directory.GetDirectories(appDir).Length.ToString();    /// Вывод общего количества папок в рабочей директории
+            toolStripStatusLabel6.Text = "0";
+            #endregion
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshInfo();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F5:
+                    btnRefresh.PerformClick();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            // Draw the background of the ListBox control for each item.
+            e.DrawBackground();
+            // Draw the current item text based on the current Font 
+            // and the custom brush settings.
+            e.Graphics.DrawString(listBox1.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds, StringFormat.GenericDefault);
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            e.DrawFocusRectangle();
+        }
+    }
+}
